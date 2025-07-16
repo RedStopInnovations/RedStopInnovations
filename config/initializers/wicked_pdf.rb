5726d76a2fd6de6db65a51c9e21a83791a811691
luanht <htluan2605@gmail.com>
2025-07-16 11:52:58 +0700

    update wkhtmltopdf path

--- config/initializers/wicked_pdf.rb
+++ config/initializers/wicked_pdf.rb
@@ -1,4 +1,4 @@
-wkhtmltopdf_path = ENV.fetch('WKHTMLTOPDF_PATH', '/usr/bin/wkhtmltopdf')
+wkhtmltopdf_path = ENV.fetch('WKHTMLTOPDF_PATH', '/usr/local/bin/wkhtmltopdf')
 WickedPdf.config = {
   exe_path: wkhtmltopdf_path
 }
